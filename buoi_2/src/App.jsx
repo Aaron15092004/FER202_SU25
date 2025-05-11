60ce2906002024a863486a7d2e7578b755260387
DESKTOP-EOK60Q1\Admin <huynhnmhe187232@fpt.edu.vn>
2025-05-12 01:59:38 +0700

    feat(layout): add responsive header and footer

--- buoi_2/src/App.jsx
+++ buoi_2/src/App.jsx
@@ -1,19 +1,25 @@
 // import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 
-import "./styles.css";
-import Button from "./components/Button";
-import CardComponent from "./components/CardComponent";
 
 function App() {
   return (
     <>
-      <div>
-        <Button colorText={"pink"} buttonVersion={"true"} />
-        <div className="mt-4"></div>
-        <Button colorText={"red"} />
-        <div className="mt-4"></div>
-        <CardComponent />
-      </div>
+      {/* Tab Header */}
+      <Header />
+
+      {/* Tab Content */}
+      <Container className="mt-3">
+        <h1>Welcome to My Website</h1>
+        <p>This is a simple example of a Bootstrap navbar with React.</p>
+        <Button variant="primary">Click Me!</Button>
+      </Container>
+
+      {/* Tab Footer */}
+      <Footer />
     </>
   );
 }
---
+++ buoi_2/src/layout/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-light text-center text-lg-start mt-auto">
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "darkcyan" }}
+        >
+          © 2025 MyWebsite. All rights reserved.
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
